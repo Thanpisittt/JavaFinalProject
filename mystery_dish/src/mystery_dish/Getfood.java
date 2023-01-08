bccce4b8cf13d09002fb17fc9b858cca3f416f86
thanpisittt <koonsho090@gmail.com>
2023-01-08 19:21:20 +0700

    Second edit

--- mystery_dish/src/mystery_dish/Getfood.java
+++ mystery_dish/src/mystery_dish/Getfood.java
@@ -6,21 +6,48 @@ public class Getfood {
 		Scanner scan =new Scanner(System.in);
 		randomfood food = new randomfood();
 		String ans ="";
-		System.out.print("Are you Muslims? y/n: "  );
-		   ans =scan.nextLine();
-		  if(ans.equalsIgnoreCase("y") ){
-				System.out.println(food.getRandomFoodhalal());
-			}
-		  else if(ans.equalsIgnoreCase("n")) {
-		      System.out.println(food.getRandomFoodnonhalal());
-			}
+		int num;
+		System.out.println("PRESS [1] Breakfast\nPRESS [2] Lunch\nPRESS [3] Dinner"  );
+		System.out.print("What meal do you want to eat : "  );
+		   num =scan.nextInt	();
+		   System.out.println();
+		 while(num < 1 || num > 3)
+		   {
+			   System.out.println("PRESS [1] Breakfast\nPRESS [2] Lunch\nPRESS [3] Dinner"  );
+				System.out.print("What meal do you want to eat : "  );
+				 num =scan.nextInt();
+		   }
+		 
+		 System.out.print("Are you Muslims? y/n: ");
+		   ans =scan.next();
 
-		while(!ans.equalsIgnoreCase("y") && !ans.equalsIgnoreCase("n")) {
-			System.out.print("Are you Muslims? y/n: "  );
-			 ans =scan.nextLine();
-			 
-		}
+			while(!ans.equalsIgnoreCase("y") && !ans.equalsIgnoreCase("n")) {
+				System.out.print("Are you Muslims? y/n: "  );
+				 ans =scan.next();
+			}
+			
+		 
+		   
+	      if(ans.equalsIgnoreCase("y")&&num == 1 ){
+				System.out.println(food.getRandomFoodhalal_B());
+			}
+		  if(ans.equalsIgnoreCase("n")&&num == 1) {
+		      System.out.println(food.getRandomFoodnonhalal_B());
+			}
+		  if(ans.equalsIgnoreCase("y")&&num == 2) {
+		      System.out.println(food.getRandomFoodhalal_L());
+			}
+		  if(ans.equalsIgnoreCase("n")&&num == 2) {
+		      System.out.println(food.getRandomFoodnonhalal_L());
+			}
+		  if(ans.equalsIgnoreCase("y")&&num == 3) {
+		      System.out.println(food.getRandomFoodhalal_D());
+			}
+		  if(ans.equalsIgnoreCase("n")&&num == 3) {
+		      System.out.println(food.getRandomFoodnonhalal_D());
+			}
 		
+
 	}
 
 }
--- mystery_dish/src/mystery_dish/randomfood.java
+++ mystery_dish/src/mystery_dish/randomfood.java
@@ -2,17 +2,41 @@ package mystery_dish;
 import java.util.Random;
 public class randomfood {
 
-	 private  String[] foodItemsnonhalal = {"กะเพราหมู", "กะเพราไก่", "หมูกระเทียม", "ข้าวไข่เจียว", "กะเพราหมูกรอบ",""};
-	 private  String[] foodItemshalal = {"ไก่กระเทียม", "กะเพราไก่", "ก๋วยเตี๋ยวไก่", "ข้าวไข่เจียว", "แกงกะหรี่ไก่ทอด","สุกี้ไก่น้ำ","มัสมันไก่"};
-	 public  String getRandomFoodnonhalal() {
+	 private  String[] foodItemsnonhalal_B = {"โจ๊กหมู", "ข้าวต้มไก่", "หมูปิ้ง", "ปลาท่องโก๋น้ำเต้าหู้", "ขนมปังแฮมชีส","แซนวิชหมูหยอง"};//มื้อเช้าแบบปกติ
+	 private  String[] foodItemsnonhalal_L = {"กะเพราหมูชิ้น", "กะเพราไก่", "หมูกระเทียม", "ข้าวไข่เจียว", "กะเพราหมูกรอบ","สเต็กporkchop"};//มื้อเที่ยงแบบปกติ
+	 private  String[] foodItemsnonhalal_D = {"แกงจืดไข่น้ำ", "กะเพราหมูสับไข่ดาว", "หมูกระเทียม", "ข้าวไข่เจียวทรงเครื่อง", "น้ำตกคอหมูย่าง","หมูพริกแกง"};//มื้อเย็นแบบปกติ
+	 private  String[] foodItemshalal_B = {"โจ๊กไก่", "ข้าวต้มปลา", "ปลาท่องโก๋น้ำเต้าหู้", "ข้าวไข่เจียว", "ไก่ฝอยข้าวเหนียว","ไข่ตุ๋นเนื้อเปื่อย"};//มื้อเช้าแบบฮาลาล
+	 private  String[] foodItemshalal_L = {"ไก่กระเทียม", "กะเพราไก่", "ก๋วยเตี๋ยวไก่", "ข้าวไข่เจียว", "แกงกะหรี่ไก่ทอด","สุกี้ไก่น้ำ","มัสมันไก่","ข้าวมันไก่"};//มื้อเที่ยงแบบฮาลาล
+	 private  String[] foodItemshalal_D = {"ไก่ผัดขิง", "แกงเขียวหวานไก่", "ต้มยำไก่ ไข่เจียว", "ไก่ทอดกระเทียม", "แกงกะหรี่ไก่ทอด",};//มื้อเย็นแบบฮาลาล
+	 public  String getRandomFoodnonhalal_B() {
 		    Random random = new Random();
-		    int index = random.nextInt(foodItemsnonhalal.length);
-		    return foodItemsnonhalal[index];
+		    int index = random.nextInt(foodItemsnonhalal_B.length);
+		    return foodItemsnonhalal_B[index];
 		  }
-	 public  String getRandomFoodhalal() {
+	 public  String getRandomFoodnonhalal_L() {
 		    Random random = new Random();
-		    int index = random.nextInt(foodItemshalal.length);
-		    return foodItemshalal[index];
+		    int index = random.nextInt(foodItemsnonhalal_L.length);
+		    return foodItemsnonhalal_L[index];
+		  }
+	 public  String getRandomFoodnonhalal_D() {
+		    Random random = new Random();
+		    int index = random.nextInt(foodItemsnonhalal_D.length);
+		    return foodItemsnonhalal_D[index];
+		  }
+	 public  String getRandomFoodhalal_B() {
+		    Random random = new Random();
+		    int index = random.nextInt(foodItemshalal_B.length);
+		    return foodItemshalal_B[index];
+		  }
+	 public  String getRandomFoodhalal_L() {
+		    Random random = new Random();
+		    int index = random.nextInt(foodItemshalal_L.length);
+		    return foodItemshalal_L[index];
+		  }
+	 public  String getRandomFoodhalal_D() {
+		    Random random = new Random();
+		    int index = random.nextInt(foodItemshalal_D.length);
+		    return foodItemshalal_D[index];
 		  }
 }
 
